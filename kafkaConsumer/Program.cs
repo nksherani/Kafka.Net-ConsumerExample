8d6d6572347dee3051f8d98748f7a20821bd0fe1
Naveed Ahmed Khan <nksherani@outlook.com>
2020-08-26 22:19:06 +0500

    initial commit

---
+++ kafkaConsumer/Program.cs
@@ -0,0 +1,19 @@
+﻿using ConsoleApp1.Confluent;
+using ConsoleApp1.kafka_net;
+using KafkaNet;
+using KafkaNet.Model;
+using System;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //KafkaNetConsumer.StartConsumer();
+            ConfluentConsumer consumer = new ConfluentConsumer();
+            consumer.SubscribeAsync("test-topic", Console.WriteLine);
+        }
+    }
+}
---
+++ kafkaConsumer/kafka-net/KafkaNetConsumer.cs
@@ -0,0 +1,31 @@
+﻿using KafkaNet;
+using KafkaNet.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.kafka_net
+{
+    class KafkaNetConsumer
+    {
+        public static void StartConsumer()
+        {
+            string topic = "test-topic";
+            Uri uri = new Uri("http://localhost:9092");
+            var options = new KafkaOptions(uri);
+
+            var router = new BrokerRouter(options);
+
+            var consumer = new Consumer(new ConsumerOptions(topic, router));
+            //consumer.SetOffsetPosition(new KafkaNet.Protocol.OffsetPosition(0, 10));
+
+            while (true)
+            {
+                foreach (var message in consumer.Consume())
+                {
+                    Console.WriteLine(Encoding.UTF8.GetString(message.Value));
+                }
+            }
+        }
+    }
+}
